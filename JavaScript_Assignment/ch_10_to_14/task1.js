36cc0dbff16d98696ad6891f1d130753b1735360
mh-saeed <saeedkhan111244@gmail.com>
2021-02-26 18:24:26 +0500

    Update task1.js

--- JavaScript_Assignment/ch_10_to_14/task1.js
+++ JavaScript_Assignment/ch_10_to_14/task1.js
@@ -8,23 +8,76 @@ let num1 = parseFloat(prompt(`Enter 1st number`));
 
 let num2 = parseFloat(prompt(`Enter 2nd number`));
 
-let operation = prompt(`Enter operation i.e (+, -, /, *)`);
-if (operation != `+` && operation != `-` && operation != `*` && operation != `/`) {
-    window.alert(`Invalid entry! \n please enter only numbers in fields \nand enter operator from follwing (+, -, /, *)`);
-}
-else if (operation === `+` && (typeof num1) == `number` && (typeof num2) == `number`) {
-    window.alert(`Addition of ${num1 + operation + num2} is: ` + (num1 + num2));
-}
-else if (operation === `-` && num1 && num2 && (typeof num1) == `number` && (typeof num2) == `number`) {
-    window.alert(`Subtraction of ${num1 + operation + num2} is: ` + (num1 - num2));
-}
-else if (operation === `*` && num1 && num2 && (typeof num1) == `number` && (typeof num2) == `number`) {
-    window.alert(`Multiplication of ${num1 + operation + num2} is: ` + (num1 * num2));
-}
-else if (operation === `/` && (typeof num1) == `number` && (typeof num2) == `number`) {
-    window.alert(`Division of ${num1 + operation + num2} is: ` + (num1 / num2));
-}
-else {
-    window.alert(`Something is wrong! \n Try again! `)
 
+if (num1.toString() === `NaN` || num2.toString() === `NaN`) {
+    window.alert(`number field is empty! \n Please enter number first.`)
+    let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+    if (conf) {
+        location.reload();
+    }
+}
+else if (num1.toString() !== `NaN` && num2.toString() !== `NaN`) {
+
+    let operation = prompt(`Enter operation i.e (+, -, /, *)`);
+
+    if (operation != `+` && operation != `-` && operation != `*` && operation != `/`) {
+        window.alert(`Invalid entry! \n please enter only numbers in fields \nand enter operator from follwing (+, -, /, *)`);
+
+        let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+        if (conf) {
+            location.reload();
+        }
+    }
+    else if (operation === `+` && (typeof num1) == `number` && (typeof num2) == `number`) {
+
+        window.alert(`Addition of ${num1 + operation + num2} is: ` + (num1 + num2));
+
+        let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+        if (conf) {
+            location.reload();
+        }
+    }
+    else if (operation === `-` && num1 && num2 && (typeof num1) == `number` && (typeof num2) == `number`) {
+
+        window.alert(`Subtraction of ${num1 + operation + num2} is: ` + (num1 - num2));
+
+        let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+        if (conf) {
+            location.reload();
+        }
+    }
+    else if (operation === `*` && num1 && num2 && (typeof num1) == `number` && (typeof num2) == `number`) {
+
+        window.alert(`Multiplication of ${num1 + operation + num2} is: ` + (num1 * num2));
+
+        let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+        if (conf) {
+            location.reload();
+        }
+    }
+    else if (operation === `/` && (typeof num1) == `number` && (typeof num2) == `number`) {
+        window.alert(`Division of ${num1 + operation + num2} is: ` + (num1 / num2));
+
+        let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+        if (conf) {
+            location.reload();
+        }
+
+    }
+    else {
+
+        window.alert(`Something is wrong! \n Try again! `);
+
+        let conf = confirm(`Do you want to try again?`); // will give true if user click on ok and false if user click on cancel
+
+        if (conf) {
+            location.reload();
+        }
+    }
 }
