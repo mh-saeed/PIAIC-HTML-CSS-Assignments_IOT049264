388b694e3c7d55deb42fab712513ec679d450061
mh-saeed <saeedkhan111244@gmail.com>
2021-03-03 00:06:06 +0500

    Update task1.js

--- JavaScript_Assignment/ch_21_30/task1.js
+++ JavaScript_Assignment/ch_21_30/task1.js
@@ -6,19 +6,16 @@ Declear a variable of string and assign some text var text="This is my dummy tex
 Slice the text into a new variable from 0 to the rounded number 0 to 4; Example: text.slice(0, 4) | sliceText = This
 Use for loop and concatination to reverse the sliced string; 0==>4 ==> This ===> shiT
 print value in alert "shiT"
-Second Assignment
-input value from user in String var inputValue="cloUd naTive computinG"
-Convert every first letter of the content in Capitalize case. inputValue="Cloud Native Computing"
-print value in console.log() console.log(inputValue) | check result in browser console */
+*/
 
 let input = parseFloat(prompt(`Enter decimal number\n(e.g. 3.4, 5.1, ...)`));
 let inputG = Math.ceil(input);
 let dummyText = `This is my dummy text`;
 
 let slicedText = dummyText.slice(0, inputG);
-let reverseText= " ";
+let reverseText = "";
 
-for (let i = slicedText.length; i >= 0; i--) {
+for (let i = slicedText.length - 1; i >= 0; i--) {
     reverseText += slicedText[i];
 }
 console.log(reverseText);
